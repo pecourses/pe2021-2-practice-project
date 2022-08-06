413540484fa115cf0562ca27c98e11bd83feafc0
Viktoriia <pecourses>
2022-08-06 17:57:22 +0300

    Change ports to local run

--- server/server.js
+++ server/server.js
@@ -3,9 +3,10 @@ const app = require('./app');
 const controller = require('./socketInit');
 require('./dbMongo/mongoose');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+
 server.listen(PORT, () =>
   console.log(`Example app listening on port ${PORT}!`),
 );
