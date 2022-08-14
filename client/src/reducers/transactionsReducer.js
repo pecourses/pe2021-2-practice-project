9a55a136c8c09a4184239de04f9f9fa0b110b9d9
Viktoriia <pecourses>
2022-08-14 18:03:09 +0300

    Add transactions reducer

--- client/src/reducers/transactionsReducer.js
+++ client/src/reducers/transactionsReducer.js
@@ -1,3 +1,5 @@
+import ACTION from '../actions/actionTypes';
+
 const initialState = {
   transactions: [],
   isFetching: false,
@@ -7,6 +9,28 @@ const initialState = {
 const transactionsReducer = (state = initialState, action) => {
   const { type } = action;
   switch (type) {
+    case ACTION.GET_TRANSACTIONS_REQUEST: {
+      return { ...state, isFetching: true, error: null };
+    }
+    case ACTION.GET_TRANSACTIONS_SUCCESS: {
+      const { data } = action;
+
+      return {
+        ...state,
+        isFetching: false,
+        transactions: [...data],
+      };
+    }
+    case ACTION.GET_TRANSACTIONS_ERROR: {
+      const { err } = action;
+
+      return {
+        ...state,
+        isFetching: false,
+        error: err,
+      };
+    }
+
     default:
       return state;
   }
